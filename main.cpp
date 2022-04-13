c87f7aa85afe21b877284acd25b4bf2096e3c47a
surfactants <8463818+surfactants@users.noreply.github.com>
2022-04-13 19:09:40 -0400

    commented and organized

--- main.cpp
+++ main.cpp
@@ -29,48 +29,58 @@ using runClock = std::chrono::high_resolution_clock;
 int main(){
 	auto runStart = runClock::now();
 
+	//define origin directory and create target directory
 	std::string directory = "D:/Music/Music/";
+		fs::create_directory(directory + "Library/");
 
-	fs::create_directory(directory + "Library/");
+	//file counts
+	unsigned int index = 0, moveCount = 0, skipCount = 0;
 
-	unsigned int index = 0, moveCount = 0;
-
+	//primary loop, iterates through origin directory and finds songs
 	for(const auto& entry : fs::directory_iterator(directory)){
 		std::string path = entry.path().string();
 		
-		if(!goodPath(path)) continue;
-		else std::cout << ++index << ": ";
+		//checks if path leads to a valid audio file
+		std::cout << "\nChecking file " << ++index << ", '" << getFileName(path) << "': ";
+		if(!goodPath(path)){
+			std::cout << "invalid file type, skipping...";
+			skipCount++;
+			continue;
+		}
 
+		//creates and defines the song
 		Song* song = new Song(directory);
 			song->origin = path;
 			song->getInfo();
 
-		try{
-			if(song->isGood()){
+
+		//attempts to move the song if it has the proper metadata
+		if(song->isGood()){
+			try{
 				fs::rename(song->origin, song->target);
 				moveCount++;
 			}
+			catch(const fs::filesystem_error& ex){
+				std::cout << "EXCEPTION: " << ex.what();
+			}
 		}
-		catch(const fs::filesystem_error& ex){
-			std::cout << "EXCEPTION: " << ex.what();
-		}
+		else skipCount++;
 
+		//clears pointer for reuse
 		delete song;
-
-		std::cout << '\n';
 	}
 
-	std::cout << "\n\n     " << index << " files found...\n     ";
-	std::cout << moveCount << " successfully moved.\n";
+	//output file counts
+	std::cout << "\n\n     " << index << " files found...";
+	std::cout << "\n     " << skipCount << " files skipped...";
+	std::cout << "\n     " << moveCount << " successfully moved.\n";
 
+	//calculate and output runtime
 	auto runEnd = runClock::now();
-
 	auto runElapsed = runEnd - runStart;
-
 	double runTime = std::chrono::duration<double>(runElapsed).count() / 60;
-
-	std::cout << std::fixed << std::showpoint << std::setprecision(2);
-	std::cout << "     Completed in " << runTime << " minutes.\n" << std::endl;
+		std::cout << std::fixed << std::showpoint << std::setprecision(2);
+		std::cout << "     Completed in " << runTime << " minutes.\n" << std::endl;
 	
     return 0;
 }
